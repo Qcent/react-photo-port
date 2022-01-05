6af7146cafc7cb9d3ea53d809a9363ea6ecf800f
Dave Quinn <dquinn8@cogeco.ca>
2022-01-05 12:19:05 -0500

    gallery now updates photos to reflect current category

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import photo from '../../assets/small/commercial/0.jpg';
 
-function PhotoList() {
+function PhotoList({ category }) {
 
   const [photos] = useState([
     {
@@ -101,12 +100,20 @@ function PhotoList() {
     },
   ]);
 
+  const currentPhotos = photos.filter((photo) => photo.category === category);
+
   return (
     <div>
-      <img
-        src={photo}
-        alt="Commercial Example"
-      />
+      <div className="flex-row">
+        {currentPhotos.map((image, i) => (
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            key={image.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
