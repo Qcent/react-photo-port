b90b2d7d9dc727d0965b7ede8c1afc9984752ca0
Dave Quinn <dquinn8@cogeco.ca>
2022-01-06 13:30:09 -0500

    handleChange now updates the state only if form validation passes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm({ category }) {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
 
+  const [errorMessage, setErrorMessage] = useState('');
+
   function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    }
+    else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   };
 
   function handleSubmit(e) {
